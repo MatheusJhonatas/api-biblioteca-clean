674633054197e4631b404832738e6dda909ec72c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-06-24 16:11:43 -0300

    new class

---
+++ Biblioteca.Domain/Entities/Livro.cs
@@ -0,0 +1,12 @@
+namespace Biblioteca.Domain.Entities;
+
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Genero { get; set; }
+    public int AnoPublicacao { get; set; }
+    public bool Disponivel { get; set; }
+
+}

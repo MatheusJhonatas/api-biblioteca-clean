e9e65072d9c2db13f2a598b3fa756e82a30a6730
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-15 22:54:09 -0300

    add class de dominio rica

---
+++ Biblioteca.Domain/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+public sealed class Usuario
+{
+
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public string Senha { get; private set; }
+
+    public Usuario(string nome, string email, string senha)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+    }
+}

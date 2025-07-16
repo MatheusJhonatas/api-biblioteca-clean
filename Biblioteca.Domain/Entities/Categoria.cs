89b1201be53924a6fcf672d97e85313c0f5bb92d
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-16 18:44:47 -0300

    add new entite

--- Biblioteca.Domain/Entities/Categoria.cs
+++ Biblioteca.Domain/Entities/Categoria.cs
@@ -2,3 +2,13 @@
 // Atributos: Nome, Descrição.
 
 // Regras: Um livro pode ter uma ou mais categorias.
+namespace Biblioteca.Domain.Entities;
+
+public sealed class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public ICollection<Livro> Livros { get; set; } = new List<Livro>();
+}
+

1eb31abae73886dfac9eb06a504dc9fffefaaaa5
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-18 19:12:17 -0300

    add new value object

--- Biblioteca.Domain/ValueObjects/ISBN.cs
+++ Biblioteca.Domain/ValueObjects/ISBN.cs
@@ -1,3 +1,24 @@
 // Atributos: Código ISBN
 // Regras: Deve ter 10 ou 13 dígitos e seguir o padrão internacional
 // Uso: Book
+namespace Biblioteca.Domain.ValueObjects;
+public class ISBN
+{
+    #region Propriedades
+    public string Codigo { get; private set; }
+    #endregion
+
+    #region Construtores
+    public ISBN(string codigo)
+    {
+        if (string.IsNullOrWhiteSpace(codigo) || (codigo.Length != 10 && codigo.Length != 13))
+        {
+            throw new ArgumentException("O código ISBN deve ter 10 ou 13 dígitos.");
+        }
+
+        Codigo = codigo;
+    }
+    #endregion
+
+    // Regras: O código ISBN deve ser único para cada livro.
+}

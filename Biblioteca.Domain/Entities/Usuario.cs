864496b4d665110af18960274c61c7e21164ba5b
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-18 19:10:07 -0300

    add entities Leitor, Reserva
    Foi alterado também  noome da classe de usuario para leitor

--- Biblioteca.Domain/Entities/Usuario.cs
+++
@@ -1,3 +0,0 @@
-// Atributos: Nome completo (VO), Email (VO), CPF (VO), Endereço (VO), Data de Cadastro, Lista de Empréstimos
-// Regras: Pode emprestar livros, possui um limite de empréstimos ativos, pode estar inadimplente (ex: se há empréstimos atrasados)
-// Métodos: Realizar empréstimo, Devolver livro, Verificar inadimplência, Atualizar endereço

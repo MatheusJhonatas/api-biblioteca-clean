8f71547785ed2a9bf06c1ea9576cd84550fe422e
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-21 22:40:51 -0300

    add new metodos

--- Biblioteca.Domain/Entities/Entity.cs
+++ Biblioteca.Domain/Entities/Entity.cs
@@ -10,17 +10,22 @@ public abstract class Entity(Guid id) : IEquatable<Guid>, IEquatable<Entity>
     public bool Equals(Entity? other)
     {
         if (other is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        return obj.GetType() == GetType() && Equals((Entity)obj);
+        return ReferenceEquals(this, other) || Id.Equals(other.Id);
     }
     public bool Equals(Guid other) => Id == other;
     public override bool Equals(object? obj)
     {
-        if (obj is Entity otherEntity)
-        {
-            return Equals(otherEntity);
-        }
-        return false;
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Entity)obj);
     }
+    public override int GetHashCode() => Id.GetHashCode();
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        if (left is null && right is null) return true;
+        if (left is null || right is null) return false;
+        return left.Equals(right);
+    }
+    public static bool operator !=(Entity? left, Entity? right) => !(left == right);
     #endregion
 }

14c42732d27c612e6a85777eb302b845ab5f5405
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-23 23:13:56 -0300

    add new prpject de xunit

--- Biblioteca.Domain.Test/Entities/LivroTest.cs
+++ Biblioteca.Domain.Test/Entities/LivroTest.cs
@@ -1,6 +1,8 @@
 using System;
 using Biblioteca.Domain.Entities;
 using Biblioteca.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
 namespace Biblioteca.Domain.Test;
 
 public class LivroTest
@@ -12,7 +14,7 @@ public class LivroTest
         var email = new Email("marioSergioCortella@gmail.com");
         var dataNascimento = new DateTime(1964, 3, 14);
         var autor = new Autor(nomeCompleto, email, dataNascimento);
-        var isbn = new ISBN("978-3-16-148410-0");
+        var isbn = new ISBN("1234567890");
         var categorias = new List<Categoria> { new Categoria(Enums.ETipoCategoria.Romance) };
         return new Livro("A sorte segue a coragem.", autor, isbn, 2008, 23, categorias);
     }
@@ -31,4 +33,18 @@ public class LivroTest
         );
     }
     #endregion
+    [Fact]
+    public void Dado_Um_LivroIndisponivel_Deve_LancarExcecaoAoEmprestar()
+    {
+        //Arrange
+        var livro = Criar_Livro_Valido();
+        livro.Emprestar();//agora está indisponivel
+
+        //Act
+        Action act = () => livro.Emprestar();
+
+        //Assert
+        act.Should().Throw<InvalidOperationException>()
+        .WithMessage("O livro já está emprestado.");
+    }
 }

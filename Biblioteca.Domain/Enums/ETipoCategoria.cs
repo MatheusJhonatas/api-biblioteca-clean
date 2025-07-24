47f12d33b66cea5064351289a361255516a813dd
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-23 22:49:31 -0300

    add factory method

---
+++ Biblioteca.Domain/Enums/ETipoCategoria.cs
@@ -0,0 +1,12 @@
+namespace Biblioteca.Domain.Enums;
+
+public enum ETipoCategoria
+{
+    Romance = 1,
+    Ficcao = 2,
+    Biografia = 3,
+    Historia = 4,
+    Ciencia = 5,
+    AutoAjuda = 6,
+    Outra = 7
+}
--- Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
+++ Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e669b2fd4ee848ab556bf748a753498a8bfb80ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b6d8c7056fc3b46640172efa4273fc829162b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

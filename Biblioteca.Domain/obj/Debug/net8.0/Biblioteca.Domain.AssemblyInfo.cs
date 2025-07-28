d2e05cefdba103202309a3e7f59d295e6be6289a
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-28 19:07:07 -0300

    new guid no leitor

--- Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
+++ Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d6fe79351446aa786929f282a33d309eb3320b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9e66b7d405f6fd71f1377260e2e583188d42c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

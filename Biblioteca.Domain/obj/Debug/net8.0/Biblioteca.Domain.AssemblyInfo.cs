00d6fe79351446aa786929f282a33d309eb3320b
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-07-28 17:59:04 -0300

    cache

--- Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
+++ Biblioteca.Domain/obj/Debug/net8.0/Biblioteca.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e660d724e6271d09c50dc0484bfea1df3eed0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d528e2eafd3074f792fca7d471a98afeecb225d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

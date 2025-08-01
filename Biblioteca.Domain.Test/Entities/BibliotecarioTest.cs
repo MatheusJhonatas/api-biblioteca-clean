1943b062ca6037f9aff9cb71ec1d57482a04407c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-01 18:57:16 -0300

    add teste unitario para bibliotecario

---
+++ Biblioteca.Domain.Test/Entities/BibliotecarioTest.cs
@@ -0,0 +1,35 @@
+using Biblioteca.Domain.Entities;
+using Biblioteca.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Biblioteca.Domain.Test.Entites;
+
+public class BibliotecarioTest
+{
+    #region Metodos de Facbrica
+    private Bibliotecario CriarBibliotecarioValido()
+    {
+        var nome = new NomeCompleto("Henrique", "Silva");
+        var email = new Email("henriquesilva@hotmail.com");
+        var matricula = "HEN789";
+        var cargo = "Supervisor";
+        return new Bibliotecario(nome, email, matricula, cargo);
+    }
+    #endregion
+    #region Testes Unitários
+    [Fact]
+    public void Dado_Bibliotecario_Valido_Deve_Possuir_Dados_Corretos()
+    {
+        //Arrange
+        var bibliotecario = CriarBibliotecarioValido();
+        //Act Execute a ação que está sendo testada (chame o método ou função).
+        //Assert Verifique se o resultado está correto (use métodos como Assert.Equal, Assert.True etc);
+        bibliotecario.NomeCompleto.PrimeiroNome.Should().Be("Henrique");
+        bibliotecario.NomeCompleto.UltimoNome.Should().Be("Silva");
+        bibliotecario.Email.EnderecoEmail.Should().Be("henriquesilva@hotmail.com");
+        bibliotecario.Matricula.Should().Be("HEN789");
+        bibliotecario.Cargo.Should().Be("Supervisor");
+    }
+    #endregion
+}

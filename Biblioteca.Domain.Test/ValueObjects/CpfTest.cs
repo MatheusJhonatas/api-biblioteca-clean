2443fe9529e00720071ffb2ede64397b9f3cacdf
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-06 10:10:26 -0300

    add vo testes

--- Biblioteca.Domain.Test/ValueObjects/CpfTest.cs
+++ Biblioteca.Domain.Test/ValueObjects/CpfTest.cs
@@ -1,1 +1,29 @@
+using Biblioteca.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
 namespace Biblioteca.Domain.Test.ValueObjects;
+
+public class CpfTest
+{
+    [Fact]
+    public void Deve_Criar_Cpf_Valido()
+    {
+        // Arrange
+        var cpf = new CPF("52998224725");
+
+        // Act & Assert
+        cpf.Should().NotBeNull();
+        cpf.Numero.Should().Be("52998224725");
+    }
+
+    [Fact]
+    public void Nao_Deve_Criar_Cpf_Invalido()
+    {
+        // Arrange
+        Action action = () => new CPF("12345678901");
+
+        // Act & Assert
+        action.Should().Throw<ArgumentException>().WithMessage("CPF inválido*");
+    }
+}

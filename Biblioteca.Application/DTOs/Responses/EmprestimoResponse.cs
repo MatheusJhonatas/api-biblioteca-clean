68f08b4771e65cd9828b68070e8aa070284eca1d
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-08 11:31:25 -0300

    Update EmprestimoResponse.cs

--- Biblioteca.Application/DTOs/Responses/EmprestimoResponse.cs
+++ Biblioteca.Application/DTOs/Responses/EmprestimoResponse.cs
@@ -1,4 +1,6 @@
 namespace Biblioteca.Application.DTOs.Responses
 {
+    //classe record pois imutável e possui um comportamento padrão para comparação.
+    //Esse use case representa a resposta de um empréstimo de livro.
     public record EmprestimoResponse(Guid EmprestimoId, DateTime DataEmprestimo, DateTime DataPrevistaDevolucao);
 }

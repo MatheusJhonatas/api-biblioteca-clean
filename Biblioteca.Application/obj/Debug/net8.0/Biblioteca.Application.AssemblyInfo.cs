fb6cbbe3208bd80119f29f995ea864f03ec467cd
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-08 11:21:47 -0300

    add classes use case

--- Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
+++ Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14d3cde69aca3b541b23fcd976af2e52b9ac0b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c5a1e2cb2f4f0eda244a23233fe92c3f3b5fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

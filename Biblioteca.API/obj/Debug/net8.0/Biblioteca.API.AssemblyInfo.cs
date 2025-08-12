930bfddf895b29d7680e79a321541a354b72179f
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-12 19:19:43 -0300

    develop

--- Biblioteca.API/obj/Debug/net8.0/Biblioteca.API.AssemblyInfo.cs
+++ Biblioteca.API/obj/Debug/net8.0/Biblioteca.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5e75703d909b25b39e0e6481680c92b40e90e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af17180bef2bb205b386c77e8c82310918a4609f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
+++ Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f79d6461ed6f83574dafc9dc3e15e30d87ca19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af17180bef2bb205b386c77e8c82310918a4609f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

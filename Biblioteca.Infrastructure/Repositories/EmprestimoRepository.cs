825a489b641d2f4e1512e1fc7147261c18d0148c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-11 22:45:47 -0300

    add emprestimo repository

---
+++ Biblioteca.Infrastructure/Repositories/EmprestimoRepository.cs
@@ -0,0 +1,37 @@
+using Biblioteca.Domain.Interfaces;
+using Biblioteca.Infrastructure.Persistense;
+using Microsoft.EntityFrameworkCore;
+
+public class EmprestimoRepository : IEmprestimoRepository
+{
+    private readonly BibliotecaDbContext _context;
+
+    public EmprestimoRepository(BibliotecaDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Atualizar(Emprestimo emprestimo)
+    {
+        _context.Emprestimos.Update(emprestimo);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<Emprestimo> ObterAtivosPorLeitor(Guid usuarioId)
+    {
+        return _context.Emprestimos
+            .Where(e => e.UsuarioId == usuarioId && e.DataRealDevolucao == null)
+            .ToList();
+    }
+
+    public Emprestimo ObterPorId(Guid id)
+    {
+        return _context.Emprestimos.Find(id);
+    }
+
+    public void Salvar(Emprestimo emprestimo)
+    {
+        _context.Emprestimos.Add(emprestimo);
+        _context.SaveChanges();
+    }
+}

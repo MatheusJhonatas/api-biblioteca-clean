5eaf56c5cf34548c4759a3dc3f78ad3677cefd6b
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-25 17:49:06 -0300

    Add controller de Emprestimo

---
+++ Biblioteca.API/Controllers/EmprestimosController.cs
@@ -0,0 +1,34 @@
+using Biblioteca.Application.DTOs.Requests;
+using Biblioteca.Application.DTOs.Responses;
+using Biblioteca.Application.UseCases.Emprestimos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.API.Controllers;
+
+[ApiController]
+public class EmprestimosController : ControllerBase
+{
+    private readonly EmprestarLivroUseCase _emprestimoLivro;
+
+    public EmprestimosController(EmprestarLivroUseCase emprestimoLivro)
+    {
+        _emprestimoLivro = emprestimoLivro;
+    }
+
+    [HttpPost("v1/emprestimos")]
+    public async Task<IActionResult> RealizarEmprestimo([FromBody] EmprestarLivroRequest request)
+    {
+        try
+        {
+            if (request == null)
+                return BadRequest("Dados do empréstimo não podem ser nulos.");
+
+            var resultado = await _emprestimoLivro.ExecuteAsync(request);
+            return Ok(resultado);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ResultResponse<string>.Fail($"Erro interno ao emprestar um livro: {ex.Message}"));
+        }
+    }
+}

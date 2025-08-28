13fa227ba65948b394a7e878f429f47b66f5540a
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-28 18:34:50 -0300

    develop

--- Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
+++ Biblioteca.Application/obj/Debug/net8.0/Biblioteca.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e57868e67060f950ca472cad86f3b000a0fa9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a4da5a770de8076136913227b598d1f2d26716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

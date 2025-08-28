781b68e30b9eba8c0571ed323d13220ed6d118e2
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-28 18:44:32 -0300

    add controller post leitor

--- Biblioteca.API/Controllers/LeitoresController.cs
+++ Biblioteca.API/Controllers/LeitoresController.cs
@@ -1,3 +1,8 @@
+using Azure.Core;
+using Biblioteca.Application.DTOs.Requests;
+using Biblioteca.Application.DTOs.Responses;
+using Biblioteca.Application.UseCases.Leitores;
+using Biblioteca.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca.API.Controllers;
@@ -6,5 +11,32 @@ namespace Biblioteca.API.Controllers;
 [Route("api/[controller]")]
 public class LeitoresController : ControllerBase
 {
+    private readonly CadastrarLeitorUseCase _cadastrarLeitor;
+
+    public LeitoresController(CadastrarLeitorUseCase cadastrarLeitor)
+    {
+        _cadastrarLeitor = cadastrarLeitor;
+    }
+
     // Métodos da API
+    [HttpPost("v1/leitores")]
+    public async Task<IActionResult> CriarLeitor([FromBody] CadastrarLeitorRequest _cadastrarLeitorRequest)
+    {
+        try
+        {
+            if (_cadastrarLeitorRequest == null)
+                return BadRequest(ResultResponse<string>.Fail("Dados do leitor não podem ser nulos."));
+
+            var resultado = await _cadastrarLeitor.Execute(_cadastrarLeitorRequest);
+
+            if (!resultado.Success)
+                return BadRequest(resultado);
+
+            return Ok(resultado);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Erro interno ao criar leitor: {ex.Message}");
+        }
+    }
 }

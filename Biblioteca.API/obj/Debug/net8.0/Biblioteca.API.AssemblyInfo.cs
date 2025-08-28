f4a4da5a770de8076136913227b598d1f2d26716
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-08-28 11:18:43 -0300

    develop

--- Biblioteca.API/obj/Debug/net8.0/Biblioteca.API.AssemblyInfo.cs
+++ Biblioteca.API/obj/Debug/net8.0/Biblioteca.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263200242f385af7aa1b4cebc9268ad92de0b121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e57868e67060f950ca472cad86f3b000a0fa9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

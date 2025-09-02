4f7a14a24132dec38da6a126cc7935d5abd2747c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-09-02 20:17:45 -0300

    develop

--- Biblioteca.Infrastructure/obj/Debug/net8.0/Biblioteca.Infrastructure.AssemblyInfo.cs
+++ Biblioteca.Infrastructure/obj/Debug/net8.0/Biblioteca.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57ed5cfd948a0b6db05a0f9e2cb9f308f5d6ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ca69fb53064d3694d78b41daca9e46ecbdc887")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

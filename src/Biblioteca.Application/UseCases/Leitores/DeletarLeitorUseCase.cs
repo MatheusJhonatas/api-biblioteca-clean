1d74d30177bfafa1bba257d0f383245a4e73d609
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-09-10 23:43:42 -0300

    add injeção de dependencia

--- src/Biblioteca.Application/UseCases/Leitores/DeletarLeitorUseCase.cs
+++ src/Biblioteca.Application/UseCases/Leitores/DeletarLeitorUseCase.cs
@@ -1,4 +1,11 @@
+using Biblioteca.Domain.Interfaces;
+
 public class DeletarLeitorUseCase
 {
-
+    // Aqui é onde você consome o repositório para deletar um leitor, isso é injetado via construtor
+    private readonly ILeitorRepository _leitorRepo;
+    public DeletarLeitorUseCase(ILeitorRepository leitorRepo)
+    {
+        _leitorRepo = leitorRepo;
+    }
 }

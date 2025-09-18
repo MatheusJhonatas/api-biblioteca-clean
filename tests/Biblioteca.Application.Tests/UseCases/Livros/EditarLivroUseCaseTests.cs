9566161141ea0d019d6043362a0182ad4f2ade68
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-09-17 22:35:38 -0300

    add class editarLivro tests

---
+++ tests/Biblioteca.Application.Tests/UseCases/Livros/EditarLivroUseCaseTests.cs
@@ -0,0 +1,17 @@
+using Biblioteca.Application.UseCases.Livros;
+using Biblioteca.Domain.Interfaces;
+using Moq;
+
+namespace Biblioteca.Application.Tests.UseCases.Livros;
+
+public class EditarLivroUseCaseTests
+{
+    // Implementar testes unitários para EditarLivroUseCase
+    private readonly Mock<ILivroRepository> _livroRepositoryMock;
+    private readonly EditarLivroUseCase _editarLivroUseCase;
+    public EditarLivroUseCaseTests()
+    {
+        _livroRepositoryMock = new Mock<ILivroRepository>();
+        _editarLivroUseCase = new EditarLivroUseCase(_livroRepositoryMock.Object);
+    }
+}

64ce6ed69b1b37df74718f203bc0a484a6df6f82
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-09-19 19:53:40 -0300

    add ctor

--- tests/Biblioteca.Application.Tests/UseCases/Leitor/CadastrarLeitorUseCaseTests.cs
+++ tests/Biblioteca.Application.Tests/UseCases/Leitor/CadastrarLeitorUseCaseTests.cs
@@ -1,7 +1,14 @@
+using Biblioteca.Domain.Interfaces;
+using Moq;
+
 namespace Biblioteca.Application.Tests.UseCases.Leitor;
 
 public class CadastrarLeitorUseCaseTests
 {
+    private readonly Mock<ILeitorRepository> _leitorRepositoryMock;
 
-
+    public CadastrarLeitorUseCaseTests()
+    {
+        _leitorRepositoryMock = new Mock<ILeitorRepository>();
+    }
 }

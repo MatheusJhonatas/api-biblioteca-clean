964f80effb65a2901e63f3b0c9172b335a29fb17
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-09-18 21:19:20 -0300

    add metodo obter po id

--- tests/Biblioteca.Application.Tests/UseCases/Livros/ObterPorIdUseCaseTests.cs
+++ tests/Biblioteca.Application.Tests/UseCases/Livros/ObterPorIdUseCaseTests.cs
@@ -1,6 +1,32 @@
+using Biblioteca.Application.UseCases.Livros;
+using Biblioteca.Domain.Interfaces;
+using Moq;
+
 namespace Biblioteca.Application.Test.UseCases.Livros
 {
     public class ObterLivroPorIdUseCaseTest
     {
+        private readonly Mock<ILivroRepository> _livroRepoMock;
+        private readonly ObterLivroPorIdUseCase _useCase;
+        public ObterLivroPorIdUseCaseTest()
+        {
+            _livroRepoMock = new Mock<ILivroRepository>();
+            _useCase = new ObterLivroPorIdUseCase(_livroRepoMock.Object);
+        }
+        [Fact]
+        public async Task Se_Livro_for_Null_Retorna_Fail()
+        {
+            // Arrange
+            var livroId = Guid.NewGuid();
+            _livroRepoMock.Setup(repo => repo.ObterPorIdAsync(livroId))
+                          .ReturnsAsync((Biblioteca.Domain.Entities.Livro?)null);
+
+            // Act
+            var result = await _useCase.ExecuteAsync(livroId);
+
+            // Assert
+            Assert.False(result.Success);
+            Assert.Equal("Livro não encontrado, verifique o id digitado..", result.Message);
+        }
     }
 }

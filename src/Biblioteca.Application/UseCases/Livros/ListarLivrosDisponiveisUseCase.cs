1fabaaef852239264c2dbd5fb7ee32ef6613f675
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-14 22:31:49 -0300

    change namespaces use cases Livros

--- src/Biblioteca.Application/UseCases/Livros/ListarLivrosDisponiveisUseCase.cs
+++ src/Biblioteca.Application/UseCases/Livros/ListarLivrosDisponiveisUseCase.cs
@@ -1,20 +1,19 @@
 using Biblioteca.Application.DTOs.Responses;
 using Biblioteca.Domain.Interfaces;
 
-namespace Biblioteca.Application.UseCases.Livros
-{
-    public class ListarLivrosDisponiveisUseCase
-    {
-        private readonly ILivroRepository _livroRepo;
-        public ListarLivrosDisponiveisUseCase(ILivroRepository livroRepo)
-        {
-            _livroRepo = livroRepo;
-        }
+namespace Biblioteca.Application.UseCases.Livros;
 
-        public async Task<IEnumerable<LivroResponse>> ExecuteAsync()
-        {
-            var livros = await _livroRepo.ListarDisponiveisAsync();
-            return livros.Select(l => new LivroResponse(l.Id, l.Titulo, l.Autor.NomeCompleto.ToString(), l.AnoPublicacao, l.Disponivel, l.NumeroPaginas, l.Descricao));
-        }
+public class ListarLivrosDisponiveisUseCase
+{
+    private readonly ILivroRepository _livroRepo;
+    public ListarLivrosDisponiveisUseCase(ILivroRepository livroRepo)
+    {
+        _livroRepo = livroRepo;
+    }
+
+    public async Task<IEnumerable<LivroResponse>> ExecuteAsync()
+    {
+        var livros = await _livroRepo.ListarDisponiveisAsync();
+        return livros.Select(l => new LivroResponse(l.Id, l.Titulo, l.Autor.NomeCompleto.ToString(), l.AnoPublicacao, l.Disponivel, l.NumeroPaginas, l.Descricao));
     }
 }
--- src/Biblioteca.Application/UseCases/Livros/ObterLivroPorIdUseCase.cs
+++ src/Biblioteca.Application/UseCases/Livros/ObterLivroPorIdUseCase.cs
@@ -1,43 +1,42 @@
 using Biblioteca.Application.DTOs.Responses;
 using Biblioteca.Domain.Interfaces;
+namespace Biblioteca.Application.UseCases.Livros;
 
-namespace Biblioteca.Application.UseCases.Livros
+public class ObterLivroPorIdUseCase
 {
-    public class ObterLivroPorIdUseCase
+    // Dependência do repositório de livros
+    private readonly ILivroRepository _livroRepo;
+    public ObterLivroPorIdUseCase(ILivroRepository livroRepo)
     {
-        // Dependência do repositório de livros
-        private readonly ILivroRepository _livroRepo;
-        public ObterLivroPorIdUseCase(ILivroRepository livroRepo)
+        _livroRepo = livroRepo;
+    }
+    //metodo async para obter livro por id
+    public async Task<ResultResponse<LivroResponse>> ExecuteAsync(Guid id)
+    {
+        try
         {
-            _livroRepo = livroRepo;
+            var livro = await _livroRepo.ObterPorIdAsync(id);
+
+            if (livro == null)
+                return ResultResponse<LivroResponse>.Fail("Livro não encontrado, verifique o id digitado..");
+
+            // // Cria o DTO de forma segura, tratando nulls nos ValueObjects
+            var response = new LivroResponse(
+                livro.Id,
+                livro.Titulo ?? "Titulo Desconhecido",
+                livro.Autor?.NomeCompleto?.ToString() ?? "Autor desconhecido",
+                livro.AnoPublicacao,
+                livro.Disponivel,
+                livro.NumeroPaginas,
+                livro.Descricao
+            );
+
+            return ResultResponse<LivroResponse>.Ok(response);
         }
-        //metodo async para obter livro por id
-        public async Task<ResultResponse<LivroResponse>> ExecuteAsync(Guid id)
+        catch (Exception ex)
         {
-            try
-            {
-                var livro = await _livroRepo.ObterPorIdAsync(id);
-
-                if (livro == null)
-                    return ResultResponse<LivroResponse>.Fail("Livro não encontrado, verifique o id digitado..");
-
-                // // Cria o DTO de forma segura, tratando nulls nos ValueObjects
-                var response = new LivroResponse(
-                    livro.Id,
-                    livro.Titulo ?? "Titulo Desconhecido",
-                    livro.Autor?.NomeCompleto?.ToString() ?? "Autor desconhecido",
-                    livro.AnoPublicacao,
-                    livro.Disponivel,
-                    livro.NumeroPaginas,
-                    livro.Descricao
-                );
-
-                return ResultResponse<LivroResponse>.Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return ResultResponse<LivroResponse>.Fail($"Erro interno ao obter livro: {ex.Message}");
-            }
+            return ResultResponse<LivroResponse>.Fail($"Erro interno ao obter livro: {ex.Message}");
         }
     }
 }
+
